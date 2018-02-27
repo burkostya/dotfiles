cc5ebdd16d9d6ce5043898d13a796503c6411b63
Konstantin Burykin <burkostya@gmail.com>
2018-02-27 12:49:17 +0300

    fish: autostart x

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -77,8 +77,8 @@ set -U fish_greeting
 # end
 
 # Start X at login
-# if status is-login
-#   if test -z "$DISPLAY" -a $XDG_VTNR = 1
-#     exec startx -- -keeptty
-#   end
-# end
+if status is-login
+  if test -z "$DISPLAY" -a $XDG_VTNR = 1
+    exec startx -- -keeptty
+  end
+end
