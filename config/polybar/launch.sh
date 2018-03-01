96b8b370875cb69dcf24887592dfc8b0837ec4a7
Konstantin Burykin <burkostya@gmail.com>
2018-03-01 21:14:41 +0300

    polybar: autodetect monitor

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,6 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+export MONITOR=$(xrandr | grep ' connected' | awk '{ print $1 }' | head -n 1)
+
 # Launch bar1 and bar2
 polybar example &
 
