53131378bdd43439d6513a214dd2cfeee605f587
Konstantin Burykin <burkostya@gmail.com>
2020-10-25 18:55:57 +0300

    fix(android): change priority in PATH for emulator

--- config/fish/conf.d/035-android-sdk.fish
+++ config/fish/conf.d/035-android-sdk.fish
@@ -1,5 +1,5 @@
 set -Ux ANDROID_SDK_ROOT /opt/android-sdk
-set -x PATH $ANDROID_SDK_ROOT/emulator $PATH
 set -x PATH $ANDROID_SDK_ROOT/platform-tools $PATH
 set -x PATH $ANDROID_SDK_ROOT/tools/bin $PATH
 set -x PATH $ANDROID_SDK_ROOT/tools $PATH
+set -x PATH $ANDROID_SDK_ROOT/emulator $PATH
