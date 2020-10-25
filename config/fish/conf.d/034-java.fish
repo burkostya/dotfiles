a4a9097d07030e8d58497476f1e295adb28cdaa2
Konstantin Burykin <burkostya@gmail.com>
2020-10-25 13:19:04 +0300

    feat: setup env variables for flutter

---
+++ config/fish/conf.d/034-java.fish
@@ -0,0 +1,2 @@
+set -Ux JAVA_OPTS '-XX:+IgnoreUnrecognizedVMOptions --add-modules java.se.ee'
+set -Ux JAVA_HOME /usr/lib/jvm/default
---
+++ config/fish/conf.d/035-android-sdk.fish
@@ -0,0 +1,5 @@
+set -Ux ANDROID_SDK_ROOT /opt/android-sdk
+set -Ux PATH $ANDROID_SDK_ROOT/emulator $PATH
+set -Ux PATH $ANDROID_SDK_ROOT/platform-tools $PATH
+set -Ux PATH $ANDROID_SDK_ROOT/tools/bin $PATH
+set -Ux PATH $ANDROID_SDK_ROOT/tools $PATH
